3bbb067c0deb4c0c7651278ef0a2883df4b075d1
ray.huang <ray.huang@rayhuangdeMacBook-Pro.local>
2024-10-25 09:47:30 +0800

    change the env file path in database

--- backend/api/database.py
+++ backend/api/database.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-cred = credentials.Certificate("/Users/ray.huang/Desktop/benClass/backend/.env")
+cred = credentials.Certificate("./.env")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
