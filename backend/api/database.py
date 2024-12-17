22201d8e5b840a6d5ce091400244e726d126382d
ray.huang <ray.huang@rayhuangdeMacBook-Pro.local>
2024-12-17 20:15:38 +0800

    test

--- backend/api/database.py
+++ backend/api/database.py
@@ -3,8 +3,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-# cred = credentials.Certificate("./.env")
-cred = credentials.Certificate(os.path.join(os.getenv('FIREBASE_CREDENTIALS_PATH', '/app/.env')))
+cred = credentials.Certificate(os.path.join(os.getenv('FIREBASE_CREDENTIALS_PATH', '/app/config.json')))
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
